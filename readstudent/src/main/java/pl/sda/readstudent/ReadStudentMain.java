de12a82e309e4f00421f826f6d94f1b84826539b
Remigiusz Zudzin <remik.zudzin@gmail.com>
2019-03-10 12:33:51 +0100

    finished

--- readstudent/src/main/java/pl/sda/readstudent/ReadStudentMain.java
+++ readstudent/src/main/java/pl/sda/readstudent/ReadStudentMain.java
@@ -7,6 +7,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author Remigiusz Zudzin
